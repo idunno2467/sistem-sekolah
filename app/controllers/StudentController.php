85709d3f87d43acf68d79fc52379b422b255a9b4
michelle03925 <michelle039@ski.sch.id>
2026-02-24 08:30:32 +0700

    wip: setup tailwind and setup views

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -4,19 +4,16 @@ class StudentController
 {
     public function index(): void
     {
-        echo '<h1>Daftar Siswa</h1>';
-        echo '<p>Menampilkan daftar siswa</p>';
+       require_once './app/views/students/index.php';
     }
 
     public function create(): void
     {
-        echo '<h1>Tambah  Siswa</h1>';
-        echo '<p>Menampilkan form tambah siswa</p>';
+       require_once './app/views/students/create.php';
     }
 
     public function show(string $id): void
     {
-        echo '<h1>Detail  Siswa</h1>';
-        echo "<p>Menampilkan form detail siswa dengan ID: {$id}</p>";
+        require_once './app/views/students/show.php';
     }
 }
