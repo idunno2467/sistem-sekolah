0203ba7e004e7ac5bca2b92c5d296bc7bcda27fd
michelle03925 <michelle039@ski.sch.id>
2026-03-31 08:38:48 +0700

    wip: implement student create pag

--- public/index.php
+++ public/index.php
@@ -10,6 +10,7 @@ $router = new Router();
 $router->add('GET', '/students', 'StudentController', 'index');
 $router->add('GET', '/students/create', 'StudentController', 'create');
 $router->add('GET', '/students/{id}', 'StudentController', 'show');
+
 $router->run();
 
 ?>
